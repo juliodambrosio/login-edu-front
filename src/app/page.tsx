b0c780dcd14f3fec702d164524483e56496ad299
julio <gaspardi.dev@gmail.com>
2023-11-17 19:09:14 -0300

    Front-end Login Structure + Login request to back-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,53 @@
+'use client';
 import Image from 'next/image'
-import styles from './page.module.css'
+import { useState } from 'react';
+import { createServerContext } from 'react';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const userLoginData = async (e:any) => {
+    try {
+    e.preventDefault();
+    
+    const response = await axios.post('http://127.0.0.1:8080/login', {email, password });
+    
+    if(response.status == 200){
+       // Lógica para tratamento de sucesso
+      console.log('Login bem-sucedido!', response.data);
+    }
+    else{
+      console.log(response.data)
+    }
+    
+  } 
+  catch (error) {
+    // Lógica para tratamento de erro
+    console.log('Falha no login:', error);
+  }
+}
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+    <div>
+      <h1>Login - Edu</h1>
+      <form onSubmit={userLoginData}>
+        <label>Email:
+          <input type="email" id='email' onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>Password:
+          <input type="password" id='password' onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
     </main>
   )
 }
+
